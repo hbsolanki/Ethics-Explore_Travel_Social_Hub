0a64498b03454f9b69abee9064a1fe897cb0b716
Hardik Solanki <136411045+hbsolanki@users.noreply.github.com>
2024-10-16 00:05:46 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/API": "https://ethics-explore-travel-social-hub.onrender.com",
+      "/API": https://ethics-explore-travel-social-hub.onrender.com,
     },
   },
   plugins: [react()],
