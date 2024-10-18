fc6016a89d981e62a118520fe3c0635be53f250e
Hardik Solanki <136411045+hbsolanki@users.noreply.github.com>
2024-10-19 00:37:40 +0530

    Update Main.py

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,9 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Routes.User import UserRouter
 from Routes.Trip import TripRouter
 
 app=FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://main.d2uzrq378xbbmn.amplifyapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(UserRouter)
 app.include_router(TripRouter)
